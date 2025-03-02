b7605fd23c8008772ed8ea7077ff1ab11ea30dc4
Francis Terrero <terrero0611@gmail.com>
2025-03-02 13:59:30 -0400

    Add logging for API endpoint environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-
+  console.log('API endpoint:', env.VITE_API_ENDPOINT);
   return {
     plugins: [react()],
     server: {
